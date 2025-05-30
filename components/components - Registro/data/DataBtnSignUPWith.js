6e1a19b41c4c4a3c8213b943245241b1f63fa40a
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-05-30 16:21:08 -0400

    Se agrego un array de datos

---
+++ components/components - Registro/data/DataBtnSignUPWith.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import { FaGoogle } from "react-icons/fa";
+import IconMicuenta from '@/../public/img - Registro/unnamed.png'
+
+export const DataBtnSignUPWith = [
+    {
+        key: 1,
+        Icon: <FaGoogle className="text-[#007AFF]"/>,
+        Titulo: 'Registrate con Google',
+    },
+    {
+        key: 1,
+        Icon: <Image src={IconMicuenta}/>,
+        Titulo: 'Registrate con Soy yo RD!',
+    }
+];
