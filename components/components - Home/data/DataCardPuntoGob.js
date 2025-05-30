4fd6e3e184101c635237cac400d88421cc11accc
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-05-30 09:02:54 -0400

    Se agrego un array de datos de las card de los puntos gob

---
+++ components/components - Home/data/DataCardPuntoGob.js
@@ -0,0 +1,23 @@
+export const DataCardPuntoGob = [
+    {
+        key: 1,
+        NombreGob: '',
+        Distancia: '',
+        Descripcion: '',
+
+    },
+    {
+        key: 2,
+        NombreGob: '',
+        Distancia: '',
+        Descripcion: '',
+        
+    },
+    {
+        key: 3,
+        NombreGob: '',
+        Distancia: '',
+        Descripcion: '',
+        
+    }
+];
