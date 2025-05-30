524dace2fba7dbd881a33d30b36e3c2d175928d0
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-05-30 09:57:01 -0400

    Se agrego es estilo a las preguntas

--- components/components - Home/SubComponents/Preguntas.jsx
+++ components/components - Home/SubComponents/Preguntas.jsx
@@ -1,13 +1,31 @@
+import { IoIosArrowForward } from "react-icons/io";
+import { DataPreguntas } from "../data/DataPreguntas";
 
 export default function Preguntas() {
-  return (
-    <div>
-        <div>
-            <button>¿Puedo agendar una cita para el mismo dia?</button>
-        </div>
-        <div>
-
+  const preguntas = DataPreguntas.map(Data =>{
+    return (
+    <div key={Data.key} className="w-[85%]">
+        <button className="
+        border 
+        flex 
+        items-center 
+        p-2 
+        w-[100%] 
+        justify-between
+        rounded-md
+        text-[18px]
+        bg-[#F4F4F4]
+        border-[#F4F4F4]
+        ">
+          <span className="font-normal" >{Data.Pregunta}</span>
+          <IoIosArrowForward className=" text-3xl text-[#007AFF]"/>
+        </button>
+        <div className="hidden">
+          <p>{Data.Respuesta}</p>
         </div>
     </div>
   )
+  })
+
+  return(preguntas)
 }
