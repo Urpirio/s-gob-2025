3bd18cf4ded0ff3b261224d1dab7748385884fa7
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-05-30 09:56:21 -0400

    Se Agrego estilo y se mejoro el renderizado

--- components/components - Home/components/Section - Preguntas.jsx
+++ components/components - Home/components/Section - Preguntas.jsx
@@ -1,16 +1,20 @@
+import { Albert_Sans } from "next/font/google";
 import EscribirPreguntas from "../SubComponents/EscribirPreguntas";
+import Preguntas from "../SubComponents/Preguntas";
+
+const albertsans = Albert_Sans()
 
 export default function SectionPreguntas() {
   return (
-    <section>
-        <div>
-            <h1>Preguntas frecuentes</h1>
+    <section className={`flex flex-col p-5 ${albertsans}`}>
+        <div className="flex justify-center p-5">
+            <h1 className="text-5xl">Preguntas frecuentes</h1>
         </div>
-        <section>
-            <div>
-                
+        <section className="flex justify-around">
+            <div className="w-[45%] flex items-center flex-col gap-2 pt-10">
+                <Preguntas/>
             </div>
-            <div>
+            <div className="w-[45%] ">
                 <EscribirPreguntas/>
             </div>
         </section>
