88a67d4d53a3d4ceda8ea1e2a34357db78c9eecd
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-05-30 09:03:48 -0400

    Se mejoro la manera de rednerizar y se hizo mas manipulable

--- components/components - Home/SubComponents/CardTramites.jsx
+++ components/components - Home/SubComponents/CardTramites.jsx
@@ -1,20 +1,43 @@
+import { DataCardTramites } from "../data/DataCardTramites";
+import { DataCardTramites2 } from "../data/DataCardTramites";
 
 export default function CardTramites() {
-  return (
-    <article className=" w-100 flex flex-col gap-5">
-        <header>
-            <div className=" p-5 bg-[#1369CC] text-white rounded-[100px] w-20 h-20 flex justify-center items-center">
-                <span className="text-5xl font-black ">1</span>
-            </div>
-        </header>
-        <main>
-            <h1 className="text-3xl">Ahorra tiempo</h1>
-            <p>
-            Centraliza todos los tramites 
-            en un solo lugar, con pasos claros, 
-            citas en linea y notificaciones automaticas.
-            </p>
-        </main>
-    </article>
-  )
-}
+
+    const Grupo1 =  DataCardTramites.map(Data =>{
+        return (
+            <article key={Data.Key} className=" w-100 flex flex-col gap-5">
+                <header>
+                    <div className=" p-5 bg-[#1369CC] text-white rounded-[100px] w-20 h-20 flex justify-center items-center">
+                        <span className="text-5xl font-black ">{Data.numero}</span>
+                    </div>
+                </header>
+                <main>
+                     <h1 className="text-3xl">{Data.titulo}</h1>
+                    <p>{Data.descripcion}</p>
+                </main>
+            </article>
+        )
+    });
+
+    const Grupo2 =  DataCardTramites2.map(Data =>{
+        return (
+            <article key={Data.Key} className=" w-100 flex flex-col gap-5">
+                <header>
+                    <div className=" p-5 bg-[#1369CC] text-white rounded-[100px] w-20 h-20 flex justify-center items-center">
+                        <span className="text-5xl font-black ">{Data.numero}</span>
+                    </div>
+                </header>
+                <main>
+                     <h1 className="text-3xl">{Data.titulo}</h1>
+                    <p>{Data.descripcion}</p>
+                </main>
+            </article>
+        )
+    });
+
+    return({
+        Grupo1:Grupo1,
+        Grupo2: Grupo2,
+    })
+  
+};
