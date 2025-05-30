084cb1c682d15d5cc6d551492e85b78abc4af739
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-05-30 09:04:51 -0400

    Se agrego un estructura en proceso del componente Preguntas

---
+++ components/components - Home/SubComponents/Preguntas.jsx
@@ -0,0 +1,13 @@
+
+export default function Preguntas() {
+  return (
+    <div>
+        <div>
+            <button>¿Puedo agendar una cita para el mismo dia?</button>
+        </div>
+        <div>
+
+        </div>
+    </div>
+  )
+}
