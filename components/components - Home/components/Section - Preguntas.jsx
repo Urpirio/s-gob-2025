6319410900415a73a3fdf96e0810256372bd1cf7
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-05-30 09:02:19 -0400

    Se agrego las bases del componente de preguntas

---
+++ components/components - Home/components/Section - Preguntas.jsx
@@ -0,0 +1,19 @@
+import EscribirPreguntas from "../SubComponents/EscribirPreguntas";
+
+export default function SectionPreguntas() {
+  return (
+    <section>
+        <div>
+            <h1>Preguntas frecuentes</h1>
+        </div>
+        <section>
+            <div>
+                
+            </div>
+            <div>
+                <EscribirPreguntas/>
+            </div>
+        </section>
+    </section>
+  )
+}
