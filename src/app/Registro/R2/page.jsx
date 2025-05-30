ff4a5dcc887a686022d6351f50ccd637d3a2ad94
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-05-30 16:24:04 -0400

    Se agrego el R2 que representa la pagina 2 dentro de registro

---
+++ src/app/Registro/R2/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SectionRegistro2 from '../../../../components/components - Registro/components/Section - Registro2'
+
+export default function page() {
+  return (
+    <section>
+        <SectionRegistro2/>
+    </section>
+  )
+}
