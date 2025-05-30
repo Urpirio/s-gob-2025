4751f8c4b6a1354f9212ed9ca2873b6dc3f6c42a
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-05-30 09:03:21 -0400

    Se agrego un array de datos para las card de tramites

---
+++ components/components - Home/data/DataCardTramites.js
@@ -0,0 +1,36 @@
+export const DataCardTramites = [
+    {
+        Key: 1,
+        numero: 1,
+        titulo: 'Ahorra tiempo',
+        descripcion: `Centraliza todos los
+        tramites en un solo lugar, con pasos
+        claros, citas en linea y notificaciones
+        automaticas.`
+    },
+    {
+        Key: 3,
+        numero: 3,
+        titulo: 'Experiencia segura y confiable',
+        descripcion: `Validacion digital, historial de tramites
+        y exportable, todo respaldado por instituciones oficiales.`
+    },
+    
+];
+
+export const DataCardTramites2 = [
+    {
+        Key: 2,
+        numero: 2,
+        titulo: 'Orientacion clara e intuitiva',
+        descripcion: `Cada uno de tus tramites
+        esta explicado de manera detallada e intuitiva.`
+    },
+    {
+        Key: 4,
+        numero: 4,
+        titulo: 'Accesibilidad para todos',
+        descripcion: `Interfaz amigable, adaptable a cualquier dispositivo,
+        pensada para ciudadanos.`
+    }
+];
