6b96734fa49667adbe1007315d499e48ad9f09b9
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-05-30 09:56:31 -0400

    Se agrego el array de datos de Las preguntas

---
+++ components/components - Home/data/DataPreguntas.js
@@ -0,0 +1,37 @@
+export const DataPreguntas = [
+    {
+        key: 1,
+        Pregunta: '¿Puedo agendar una cita para el mismo dia?',
+        Respuesta: ''
+    },
+    {
+        key: 2,
+        Pregunta: '¿Que pasa si cancelo mi cita?',
+        Respuesta: ''
+    },
+    {
+        key: 3,
+        Pregunta: '¿Puedo cambiar mi cita hacia otra gestion?',
+        Respuesta: ''
+    },
+    {
+        key: 4,
+        Pregunta: '¿Puedo agendar varias gestiones en una cita?',
+        Respuesta: ''
+    },
+    {
+        key: 5,
+        Pregunta: '¿Puedo cambiar mi cita hacia una nueva institucion?',
+        Respuesta: ''
+    },
+    {
+        key: 6,
+        Pregunta: '¿Que pasa si llego tarde?',
+        Respuesta: ''
+    },
+    {
+        key: 7,
+        Pregunta: '¿Puedo cambiar la fecha de mi cita?',
+        Respuesta: ''
+    },
+]
