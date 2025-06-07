a7a6378839ad4fe063aacb8258624aaf9163b7c7
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-07 16:54:05 -0400

    Se agrego un data Array de los modos y sus funciones

---
+++ components/components - Configuracion/Data/DataCardMode.js
@@ -0,0 +1,19 @@
+
+export const DataCardMode = [
+    {
+        key: 1,
+        BackGround: 'bg-white',
+        fuction: ()=>{
+            
+        },
+        titles: 'Modo claro'
+    },
+    {
+        key: 1,
+        BackGround: 'bg-black',
+        fuction: ()=>{
+
+        },
+        titles: 'Modo Oscuro'
+    }
+];
