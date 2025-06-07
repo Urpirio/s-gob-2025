e2756aba3385a7b970f4a6f2d620a2453a5a5320
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-07 17:30:07 -0400

    Se cambio el header por completo a una version mas optima y limpia

--- components/components - globals/Header.jsx
+++ components/components - globals/Header.jsx
@@ -1,16 +1,29 @@
+import Link from "next/link";
+import { TbWorld } from "react-icons/tb";
+import { IoIosArrowDown } from "react-icons/io";
 
 
 export default function Header() {
   return (
-    <header>
-      <div>
-
+    <header className="flex justify-between items-center shadow-2xs px-5 py-4">
+      <div className="flex flex-grow justify-start  w-[30%]">
+        <h1 className="text-2xl font-bold">LOGO</h1>
       </div>
-      <div>
-
+      <div className="hidden w-[30%] justify-center lg:flex">
+        <nav className="w-[100%]  flex justify-between">
+          <Link href={''}>Inicio</Link>
+          <Link href={''}>Servicios</Link>
+          <Link href={''}>Sobre nosotros</Link>
+          <Link href={''}>Ayuda</Link>
+        </nav>
       </div>
-      <div>
-        
+      <div className="hidden flex-grow justify-end gap-3  w-[30%] lg:flex">
+        <button className="bg-[#0088FF] py-2 px-5 rounded-2xl text-white">Iniciar sesion</button>
+        <button className="bg-[#D9D9D9] py-2 px-5 rounded-2xl text-gray-700">Regitrarse</button>
+        <button className="flex gap-1 items-center text-2xl">
+          <TbWorld/>
+          <IoIosArrowDown/>
+        </button>
       </div>
     </header>
   );
