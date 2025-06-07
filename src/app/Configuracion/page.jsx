c9af8f2339ae560590d46be102c5b59aec774eca
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-07 16:54:54 -0400

    Se agrego la  pagina de configuracion

---
+++ src/app/Configuracion/page.jsx
@@ -0,0 +1,17 @@
+import Sectionforms from "../../../components/components - Configuracion/components/section - forms";
+import SectionModo from "../../../components/components - Configuracion/components/section - Modo";
+import SectionTitlesConfiguracion from "../../../components/components - Configuracion/components/section - TitlesConfiguracion";
+import Header from "../../../components/components - globals/Header";
+
+export default function page() {
+  return (
+    <body>
+        <Header/>
+        <main className="flex flex-col justify-center p-10 gap-5">
+            <SectionTitlesConfiguracion/>
+            <SectionModo/>
+            <Sectionforms/>
+        </main>
+    </body>
+  )
+}
