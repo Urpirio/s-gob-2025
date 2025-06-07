bd64230009c434f62c55645652f07e25870ed4fd
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-07 07:28:56 -0400

    Se agrego un array de datos para cada linea de la tabla

---
+++ components/components - Historial/data/DataHistorial.js
@@ -0,0 +1,39 @@
+export const DataHistorial = [
+    { 
+        id: 1, 
+        name: 'Cambio de cédula', 
+        date: 'Miércoles 15 de Mayo', 
+        time: '10:30 AM', 
+        status: 'Activa', 
+        ticket: 'Ver ticket' },
+    { 
+        id: 2, 
+        name: 'Solicitar pasaporte', 
+        date: 'Martes 14 de Mayo', 
+        time: '09:30 AM', 
+        status: 'Fallido', 
+        ticket: 'Ver ticket' },
+    { 
+        id: 3, 
+        name: 'Cambio de cédula', 
+        date: 'Miércoles 15 de Mayo', 
+        time: '10:30 AM', 
+        status: 'Procesado', 
+        ticket: 'Ver ticket' },
+    { 
+        id: 4, 
+        name: 'Solicitar pasaporte', 
+        date: 'Martes 14 de Mayo', 
+        time: '09:30 AM', 
+        status: 'Fallido', 
+        ticket: 'Ver ticket' 
+    },
+    { 
+        id: 5, 
+        name: 'Cambio de cédula', 
+        date: 'Miércoles 15 de Mayo', 
+        time: '10:30 AM', 
+        status: 'Activa', 
+        ticket: 'Ver ticket' 
+    },
+  ];
