73ffdca6c1003da13cfc5c4aafe0b5fe2000869c
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-07 19:40:44 -0400

    Se agrego el remastes de la seccion principal de home

---
+++ components/components - Home/components/Section - InputSearch.jsx
@@ -0,0 +1,24 @@
+import { RiSearchLine } from "react-icons/ri";
+import { FaLocationDot } from "react-icons/fa6";
+
+
+//Javier no toque esto, voy a agregar nuevas funciones despues de que termine las demas partes
+export default function SectionInputSearch() {
+  return (
+    <section className="flex justify-center items-center py-10 gap-5">
+        <div className=" flex items-center gap-5 p-3 bg-[#F3F3F3] rounded-4xl">
+            <span className=" p-1 rounded-[100%] h-10 w-10 flex justify-center items-center bg-[#0088FF]">
+                <RiSearchLine className="text-white text-2xl"/>
+            </span>
+            <input type="text"  className="outline-0 h-[100%]" placeholder="Buscador"/>
+            <button className=" py-2 px-5 rounded-3xl flex items-center gap-1 bg-white">
+                <i className="text-gray-500">¿Donde estas?</i> 
+                <FaLocationDot className="text-[#0088FF]"/>
+            </button>
+        </div>
+        <div>
+            <button className="border py-3 px-10 rounded-4xl text-white bg-[#0088FF]">Agendar nueva cita</button>
+        </div>
+    </section>
+  )
+}
