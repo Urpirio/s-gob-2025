fd8b4214798b43cbb3fde4fa869a87a1aaee13eb
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-07 19:40:58 -0400

    Se agrego en otro componente La primera seccion

--- components/components - Home/components/Section - Search.jsx
+++ components/components - Home/components/Section - Search.jsx
@@ -5,8 +5,16 @@ import { IoLocationSharp } from "react-icons/io5";
 
 export default function SectionSearch() {
   return (
-     <section >
-        
-      </section>
+    <section className="py-10 flex flex-col items-center justify-center gap-5">
+      <div className="text-center w-[40%]">
+        <h1 className="text-6xl">
+          Hola, ¿Que vamos 
+          a <i className="text-[#0088FF] font-">gestionar</i> hoy?
+        </h1>
+      </div>
+      <div className="text-center w-[45%]">
+        <h2 className="text-3xl font-extralight">Gestiona tus tramites con mayor precision, mejor enfoque y sin perder tiempo.</h2>
+      </div>
+    </section>
   )
 }
