b7336aa56cf91c64de3c193e5f7e07fac9e2fef4
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-08 12:01:31 -0400

    Se agrego la fase uno del sistema de seleccion

--- components/components - servicios/components/section - servicios.jsx
+++ components/components - servicios/components/section - servicios.jsx
@@ -1,17 +1,14 @@
+import AboutServices from "../subcomponents/AboutServices";
 import CardServicios from "../subcomponents/CardServicios";
 
 export default function SectionServicios() {
   return (
-    <section>
-        <div className="flex flex-wrap justify-center py-5 gap-5 bg-[#F3F3F3]">
+    <section className="bg-[#F3F3F3] flex justify-center" >
+        <div className="flex flex-wrap justify-center py-5 gap-5  max-w-400 overflow-y-scroll h-[100vh]" style={{scrollbarWidth:'none',scrollbarColor:'blue'}}>
             <CardServicios/>
-            <CardServicios/>
-            <CardServicios/>
-            <CardServicios/>
-            <CardServicios/>
-            <CardServicios/>
-            <CardServicios/>
-            <CardServicios/>
+        </div>
+        <div className={` w-[200%] bg-white`} >
+          <AboutServices/>
         </div>
     </section>
   )
