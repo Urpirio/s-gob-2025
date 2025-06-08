1bb550f06917efad523754fdc301929345e29a24
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-08 09:39:12 -0400

    Se  optimizo el estilo

--- components/components - Home/components/Section - Servicios.jsx
+++ components/components - Home/components/Section - Servicios.jsx
@@ -6,10 +6,12 @@ const albertsans = Albert_Sans({
   subsets: ['latin-ext']
 });
 
+//Esto hay que relacionarlo con un array de datos para cuando lo conectemos a la api funciones perfectamente.
+
 export default function SectionServicios() {
   return (
     <section className='flex flex-col gap-20 pt-10 pb-10 bg-[#EFEFEF]'>
-        <div className='flex justify-center'>
+        <div className='text-center'>
             <h1  className={`text-6xl font-semibold ${albertsans.className}`} >Servicios más utilizados</h1>
         </div>
         <div className='flex gap-5 p-5 overflow-x-scroll ' style={{scrollbarWidth:'none',scrollbarColor:'blue'}} >
