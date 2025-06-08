dbf298d3b7cf9f127e0902ed3626e08712346c27
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-08 10:36:31 -0400

    Se agregaron dos nuevas secciones a la pagina servicios

--- src/app/Servicios/page.jsx
+++ src/app/Servicios/page.jsx
@@ -1,10 +1,16 @@
 import Footer from "../../../components/components - globals/Footer";
 import Header from "../../../components/components - globals/Header";
+import SectionBuscador from "../../../components/components - servicios/components/section - buscador";
+import SectionServicios from "../../../components/components - servicios/components/section - servicios";
 
 export default function Servicios() {
   return (
     <body>
         <Header/>
+        <main>
+          <SectionBuscador/>
+          <SectionServicios/>
+        </main>
         <Footer/>
     </body>
   )
