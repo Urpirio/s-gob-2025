940a3c6e678a3c70985b1f27032dfbd24a129093
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-08 19:41:58 -0400

    Update

--- src/app/AgendarCita/page.jsx
+++ src/app/AgendarCita/page.jsx
@@ -2,13 +2,10 @@
 import SectionFormulario from "../../../components/components - AgendarCita/components/Section - formulario";
 import Footer from "../../../components/components - globals/Footer";
 import Header from "../../../components/components - globals/Header";
-import { useSearchParams } from "next/navigation";
-import { useRouter } from "next/router";
+
 
 export default function page() {
 
-    // const router = useRouter();
-    const Data = useSearchParams();
 
   return (
     <body>
