93e7872b8e650e57138a78406b152a0b5247ce23
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-07 22:52:59 -0400

    Se optimizo el codigo

--- components/components - Home/components/Section - PuntoGob.jsx
+++ components/components - Home/components/Section - PuntoGob.jsx
@@ -5,14 +5,12 @@ const Albert_sans = Albert_Sans()
 
 export default function SectionPuntoGob() {
   return (
-    <section className="pt-10 pb-10 min-h-[600px] max-w-6xl mx-auto">
-        <div className="flex flex-col items-center justify-center gap-5 pt-5 pb-10">
+    <section className="pt-10 pb-10">
+        <div className="flex flex-col text-center gap-5 pt-5 pb-10">
             <h1 className={`${Albert_sans} text-6xl font-semibold`}>Instituciones afiliadas</h1>
             <span className={`${Albert_sans} font-extralight text-4xl`}>Transformando la gestión pública contigo.</span>
         </div>
-        <div className="flex justify-around">
-            <CardGob/>
-            <CardGob/>
+        <div className="flex flex-wrap justify-around">
             <CardGob/>
         </div>
     </section>
