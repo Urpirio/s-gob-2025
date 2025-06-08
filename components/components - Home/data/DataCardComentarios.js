40acee66537217a2e2ebcfdad87eae382bd63eeb
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-07 21:17:48 -0400

    Se agrego el array de datos de los comentarios

---
+++ components/components - Home/data/DataCardComentarios.js
@@ -0,0 +1,33 @@
+export const DataCardComentarios = [
+    {
+        key:1,
+        Star: 4,
+        img: 'https://i.pinimg.com/736x/9a/2a/52/9a2a527140afe3a375a47b5221004dfa.jpg',
+        name: 'Camila Ortega',
+        UserName: 'cam_ortega99',
+        Comment: `Hoy leí que las abejas pueden reconocer
+        rostros humanos… y ahora no puedo
+        dejar de pensar en eso.`,
+    },
+    {
+        key:2,
+        Star: 5,
+        img: 'https://i.pinimg.com/736x/c8/63/66/c863667f3c81576ec3b3f029fe1a4087.jpg',
+        name: 'Juan Hernandez',
+        UserName: '@juan.hernandez_',
+        Comment: `Desde que empece a gestionar mis 
+        citas desde Gestion sin limites todo
+        es mas facil.`,
+    },
+    {
+        key:3,
+        Star: 3,
+        img: 'https://i.pinimg.com/736x/76/c2/e4/76c2e44e5eec4554a6dbb73bef0a6169.jpg',
+        name: 'Elías Navarro',
+        UserName: 'elias.navv',
+        Comment: `Hoy leí que las abejas pueden
+        reconocer rostros humanos… y
+        ahora no puedo dejar de pensar
+        en eso.`,
+    },
+]
