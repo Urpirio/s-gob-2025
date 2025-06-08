a633b1f246617943f5dd1ea70986cb5c3b0099c7
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-07 23:09:12 -0400

    Se agrego un alt

--- src/app/Registro/layout.jsx
+++ src/app/Registro/layout.jsx
@@ -2,16 +2,18 @@ import Image from "next/image"
 import ImgRegistro from '@/../public/img - Registro/Sign up-rafiki 1.png';
 import { Albert_Sans } from 'next/font/google';
 
-const albertsans = Albert_Sans()
+const albertsans = Albert_Sans({
+  subsets: ['latin-ext']
+})
 
 export default function layout({children}) {
   return (
-        <body className={`flex justify-around items-center h-[100vh] ${albertsans}`}>
+        <body className={`flex justify-around items-center h-[100vh] ${albertsans.className}`}>
             
             {children}
 
             <section>
-                <Image src={ImgRegistro}/>
+                <Image src={ImgRegistro} alt=""/>
             </section>
   
         </body>
