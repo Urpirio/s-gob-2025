795638b61d54dc46bf1a1be77b31ad3f8a406d66
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-08 09:39:59 -0400

    Se restablecio el estilo

--- components/components - Home/SubComponents/CardServicios.jsx
+++ components/components - Home/SubComponents/CardServicios.jsx
@@ -2,20 +2,26 @@ import Image from "next/image";
 import LogoGOB from '@/../public/img - Home/image 4 (1).png';
 import { CiBookmark } from "react-icons/ci";
 
+
+//Porfavor no modificar esta completamente responsive. att: urpirio 
 export default function CardServicios() {
   return (
-    <article className="bg-white p-[20px]  w-[418px] items-center h-[194px] rounded-2xl shadow-2xl">
-      <header className="flex gap-5 items-center justify-between">
+    <article className=" flex flex-col w-90  min-w-80 items-center  rounded-2xl border border-gray-300 bg-white p-5">
+      <header className="flex justify-between gap-5  w-[100%]">
         <Image className="w-[143px]" src={LogoGOB} alt="Logo"></Image>
          <CiBookmark className="w-[24px] h-[24px]" />
       </header>
       <main> 
-        <h1 className="font-semibold">punto Gob-Sambil</h1>
-        <p className="w-[334px] text-[13px] text-neutral-700">Ciudadano, moderno, eficaz, eficiente, productivo, participativo y dinámico, incorporando el uso de los recursos de información para la prestación de los servicios</p>
+        <h1 className="font-semibold">Punto Gob-Sambil</h1>
+        <p className=" text-[13px] text-neutral-700">
+          Ciudadano, moderno, eficaz, eficiente, productivo,
+           participativo y dinámico, incorporando el uso de
+            los recursos de información para la prestación de los servicios
+          </p>
 
       </main>
-      <footer className="flex items-center justify-end pl-1.5">
-        <button className= " mt-4 bg-blue-500 bg-gradient-to-tl to-blue-400 from-blue-700 rounded-[4px] w-[113px] text-white ">solicitar</button>
+      <footer className="flex items-center justify-end w-[100%]">
+        <button className= " bg-blue-500  rounded-[4px] w-30 py-0.5 text-white ">Solicitar</button>
       </footer>
     </article>
   )
