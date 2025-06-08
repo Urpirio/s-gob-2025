e2e0bab8dc5b235c0f86f253015b6c9ff937ef71
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-07 20:05:28 -0400

    Se agrego un nueva propiedad al array de datos de card tramites

--- components/components - Home/data/DataCardTramites.js
+++ components/components - Home/data/DataCardTramites.js
@@ -2,6 +2,7 @@ export const DataCardTramites = [
     {
         Key: 1,
         numero: 1,
+        Stylenumero: 'bg-[#1369CC]',
         titulo: 'Ahorra tiempo',
         descripcion:`Centraliza todos los trámites en un solo lugar, 
         con pasos claros, citas en línea y notificaciones automáticas.`
@@ -9,6 +10,7 @@ export const DataCardTramites = [
     {
         Key: 3,
         numero: 3,
+        Stylenumero: 'bg-[#D9D9D9]',
         titulo: 'Experiencia segura y confiable',
         descripcion:`Validación digital, historial de trámites y exportable,
          todo respaldado por instituciones oficiales.`
@@ -20,6 +22,7 @@ export const DataCardTramites2 = [
     {
         Key: 2,
         numero: 2,
+        Stylenumero: 'bg-[#1369CC]',
         titulo:'Orientación clara e intuitiva',
         descripcion:`Cada uno de tus trámites está explicado 
         de manera detallada e intuitiva.`
@@ -27,6 +30,7 @@ export const DataCardTramites2 = [
     {
         Key: 4,
         numero: 4,
+        Stylenumero: 'bg-[#D9D9D9]',
         titulo:'Accesibilidad para todos',
         descripcion:`Interfaz amigable, adaptable a cualquier dispositivo, pensada para ciudadanos.`
     }
