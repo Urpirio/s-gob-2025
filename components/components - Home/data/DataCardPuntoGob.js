dbe21032894b15785e31172938c5b9454d406cdb
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-07 22:53:21 -0400

    Se agregaron los datos correspondientes al array de CardPuntoGoB

--- components/components - Home/data/DataCardPuntoGob.js
+++ components/components - Home/data/DataCardPuntoGob.js
@@ -1,23 +1,29 @@
 export const DataCardPuntoGob = [
     {
         key: 1,
-        NombreGob: '',
-        Distancia: '',
-        Descripcion: '',
+        NombreGob: 'Punto Gob - Sambil',
+        Distancia: '1.8km de distancia - Sambil, Santo Domingo',
+        Descripcion: `Encuentra todos los servicios del Gobierno Dominicano en 
+        un solo Portal. Acceso rápido, seguro y digital a trámites
+        y servicios para todos los ciudadanos.`,
 
     },
     {
         key: 2,
-        NombreGob: '',
-        Distancia: '',
-        Descripcion: '',
+        NombreGob: 'Punto Gob - Sambil',
+        Distancia: '1.8km de distancia - Sambil, Santo Domingo',
+        Descripcion: `Encuentra todos los servicios del Gobierno Dominicano en 
+        un solo Portal. Acceso rápido, seguro y digital a trámites
+        y servicios para todos los ciudadanos.`,
         
     },
     {
         key: 3,
-        NombreGob: '',
-        Distancia: '',
-        Descripcion: '',
+        NombreGob: 'Punto Gob - Sambil',
+        Distancia: '1.8km de distancia - Sambil, Santo Domingo',
+        Descripcion: `Encuentra todos los servicios del Gobierno Dominicano en 
+        un solo Portal. Acceso rápido, seguro y digital a trámites
+        y servicios para todos los ciudadanos.`,
         
     }
 ];
