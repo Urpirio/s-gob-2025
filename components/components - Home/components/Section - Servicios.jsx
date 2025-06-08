a3802d3e930d8b0e7763f839234c8d6a1f6f7232
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-07 23:08:38 -0400

    Se corrigio un mala practica relacionada con la colocacion de las fuentes

--- components/components - Home/components/Section - Servicios.jsx
+++ components/components - Home/components/Section - Servicios.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import CardServicios from '../SubComponents/CardServicios';
 import { Albert_Sans } from 'next/font/google'
 
-const albertsans = Albert_Sans()
+const albertsans = Albert_Sans({
+  subsets: ['latin-ext']
+});
 
 export default function SectionServicios() {
   return (
     <section className='flex flex-col gap-20 pt-10 pb-10 bg-[#EFEFEF]'>
         <div className='flex justify-center'>
-            <h1  className={`text-6xl font-semibold ${albertsans}`} >Servicios más utilizados</h1>
+            <h1  className={`text-6xl font-semibold ${albertsans.className}`} >Servicios más utilizados</h1>
         </div>
         <div className='flex gap-5 p-5 overflow-x-scroll ' style={{scrollbarWidth:'none',scrollbarColor:'blue'}} >
             <CardServicios/>
