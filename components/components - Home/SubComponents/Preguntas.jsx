32e803d131041a6256a836b2b3a180648a60db53
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-07 22:41:13 -0400

    Se optimizo u mejoro el codigo de las tarjetas de preguntas

--- components/components - Home/SubComponents/Preguntas.jsx
+++ components/components - Home/SubComponents/Preguntas.jsx
@@ -1,29 +1,28 @@
+'use client';
 import { IoIosArrowForward } from "react-icons/io";
+import { IoIosArrowDown } from "react-icons/io";
 import { DataPreguntas } from "../data/DataPreguntas";
+import { List, ListItemButton,Collapse, ListItemText } from "@mui/material";
+import { useState } from "react";
+
 
 export default function Preguntas() {
   const preguntas = DataPreguntas.map(Data =>{
+    const [Open,setOpen] = useState(false);
+
     return (
-    <div key={Data.key} className="w-[85%]">
-        <button className="
-        border 
-        flex 
-        items-center 
-        p-2 
-        w-[100%] 
-        justify-between
-        rounded-md
-        text-[18px]
-        bg-[#F4F4F4]
-        border-[#F4F4F4]
-        ">
-          <span className="font-normal" >{Data.Pregunta}</span>
-          <IoIosArrowForward className=" text-3xl text-[#007AFF]"/>
-        </button>
-        <div className="hidden">
-          <p>{Data.Respuesta}</p>
-        </div>
-    </div>
+   <div key={Data.key}>
+    <ListItemButton onClick={()=>{setOpen(!Open)}} style={{borderRadius:5,background:'#F4F4F4'}}>
+      <div className="flex items-center w-[100%]">
+         <ListItemText primary={Data.Pregunta}/>
+          <IoIosArrowForward className={`text-[#007AFF] text-2xl 
+            ${Open ? 'rotate-90' :'rotate-0'} transition-all duration-300`}/>
+      </div>
+    </ListItemButton>
+    <Collapse in={Open} className="px-5 border-l border-r border-b rounded-b-md border-gray-400">
+      <p className="text-gray-600">{Data.Respuesta}</p>
+    </Collapse>
+   </div>
   )
   })
 
