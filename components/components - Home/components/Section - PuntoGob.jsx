62c301b85c8ba2a440bb54fc034c5f8b504fb17c
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-07 23:08:15 -0400

    Se corrigio un mala practica relacionada con la colocacion de las fuentes

--- components/components - Home/components/Section - PuntoGob.jsx
+++ components/components - Home/components/Section - PuntoGob.jsx
@@ -1,14 +1,16 @@
 import { Albert_Sans } from "next/font/google";
 import CardGob from "../SubComponents/CardGob";
 
-const Albert_sans = Albert_Sans()
+const Albert_sans = Albert_Sans({
+  subsets: ['latin-ext']
+});
 
 export default function SectionPuntoGob() {
   return (
     <section className="pt-10 pb-10">
         <div className="flex flex-col text-center gap-5 pt-5 pb-10">
-            <h1 className={`${Albert_sans} text-6xl font-semibold`}>Instituciones afiliadas</h1>
-            <span className={`${Albert_sans} font-extralight text-4xl`}>Transformando la gestión pública contigo.</span>
+            <h1 className={`${Albert_sans.className} text-6xl font-semibold`}>Instituciones afiliadas</h1>
+            <span className={`${Albert_sans.className} font-extralight text-4xl`}>Transformando la gestión pública contigo.</span>
         </div>
         <div className="flex flex-wrap justify-around">
             <CardGob/>
