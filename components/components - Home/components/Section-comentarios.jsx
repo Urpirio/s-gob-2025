2ac84d48ccb761997c0e74e474b6a20527ab281c
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-07 20:39:52 -0400

    Se agrego la seccion de comentario basada al 100% EN EL DISEÑO

--- components/components - Home/components/Section-comentarios.jsx
+++ components/components - Home/components/Section-comentarios.jsx
@@ -1,15 +1,27 @@
-import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
-import Comentarios from "../SubComponents/Card-comentarios";
+import { IoIosArrowForward } from "react-icons/io";
+import { IoIosArrowBack } from "react-icons/io";
+import CardComentarios from "../SubComponents/Card-comentarios";
 
 export default function SectionComentarios() {
     return(
-        <section>
-            <div>
-                <h1></h1>
+        <section className="py-10 flex flex-col gap-10">
+            <div className="text-center">
+                <h1 className="text-4xl">“Transformando el servicio contigo”</h1>
             </div>
-            <div>
-                
+           <div className="w-[100%] flex justify-center gap-10">
+            <div className="flex items-center">
+                <button className="text-6xl text-[#007AFF]"><IoIosArrowBack/></button>
             </div>
+             <div  className="flex justify-start gap-5 ">
+                <CardComentarios/>
+                <CardComentarios/>
+                <CardComentarios/>
+            </div>
+            <div className="flex items-center" >
+                <button className="text-6xl text-[#007AFF]"><IoIosArrowForward/></button>
+            </div>
+           </div>
+           
         </section>
     )
 }
