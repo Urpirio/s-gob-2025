b391c82911f0bb95f8f6d347063e367e8617d09a
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-08 19:32:06 -0400

    Se agrego la pagina del formulario de cita

---
+++ components/components - AgendarCita/components/Section - formulario.jsx
@@ -0,0 +1,18 @@
+import FormPart1 from "../subcomponents/FormPart1";
+import FormPart2 from "../subcomponents/FormPart2";
+import FormPart3 from "../subcomponents/FormPart3";
+import { Albert_Sans } from "next/font/google";
+
+const albert_Sans = Albert_Sans({
+  subsets: ['latin-ext']
+});
+
+export default function SectionFormulario() {
+  return (
+    <section className={`flex justify-around p-10 ${albert_Sans.className}`}>
+            <FormPart2/>
+            <FormPart1/>
+            <FormPart3/>
+    </section>
+  )
+};
