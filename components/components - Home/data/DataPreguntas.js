0d3e6688c3f8ee338b49b515621a8597580eaf40
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-07 22:40:40 -0400

    Se agrego las respuestas de las preguntas

--- components/components - Home/data/DataPreguntas.js
+++ components/components - Home/data/DataPreguntas.js
@@ -2,36 +2,36 @@ export const DataPreguntas = [
     {
         key: 1,
         Pregunta: '¿Puedo agendar una cita para el mismo dia?',
-        Respuesta: ''
+        Respuesta: `Sí, siempre que haya disponibilidad en el sistema. Te recomendamos revisar temprano en la mañana.`
     },
     {
         key: 2,
         Pregunta: '¿Qué pasa si cancelo mi cita?',
-        Respuesta: ''
+        Respuesta: `No hay penalización, pero deberás esperar a que haya un nuevo espacio disponible para reagendar.`
     },
     {
         key: 3,
         Pregunta: '¿Puedo cambiar mi cita hacia otra gestión?',
-        Respuesta: ''
+        Respuesta: `Sí, puedes hacerlo desde el portal, pero solo si aún no se ha cumplido la fecha de la cita original.`
     },
     {
         key: 4,
         Pregunta: '¿Puedo agendar varias gestiones en una cita?',
-        Respuesta: ''
+        Respuesta: `En algunos casos es posible, siempre y cuando las gestiones estén relacionadas o sean del mismo departamento.`
     },
     {
         key: 5,
         Pregunta: '¿Puedo cambiar mi cita hacia una nueva institución?',
-        Respuesta: ''
+        Respuesta: `No se puede trasladar directamente. Tendrás que cancelar la cita actual y solicitar una nueva en la institución correspondiente.`
     },
     {
         key: 6,
         Pregunta: '¿Qué pasa si llego tarde?',
-        Respuesta: ''
+        Respuesta: `Si llegas con más de 10 minutos de retraso, podrían no atenderte y tendrías que agendar otra cita.`
     },
     {
         key: 7,
         Pregunta: '¿Puedo cambiar la fecha de mi cita?',
-        Respuesta: ''
+        Respuesta: `Sí, puedes modificarla desde el sistema en línea, siempre que lo hagas al menos 24 horas antes de la cita programada.`
     },
 ]
