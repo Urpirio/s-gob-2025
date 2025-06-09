a3029c66a78077f1590b40ba15f9eeaa3369ca70
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-08 21:51:02 -0400

    Se agrego la pagina del Login

--- src/app/Login/page.jsx
+++ src/app/Login/page.jsx
@@ -1,6 +1,18 @@
+import Header from "../../../components/components - globals/Header";
+import SectionLogin from "../../../components/components - Login/components/Section - Login";
+import ImgRegistro from '@/../public/img - Login/Sign up-rafiki 1.png';
+import Image from "next/image";
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <body>
+      {/* <Header/> */}
+      <main className="flex justify-around items-center h-[100vh]">
+        <SectionLogin/>
+         <section>
+            <Image src={ImgRegistro} alt=""/>
+        </section>
+      </main>
+    </body>
   )
 }
