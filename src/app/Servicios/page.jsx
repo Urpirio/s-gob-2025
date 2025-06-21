ef620288e2e7c39c55e1adbe843a591bbab9059e
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-21 18:11:25 -0400

    Se le cambio el path al footer y header

--- src/app/Servicios/page.jsx
+++ src/app/Servicios/page.jsx
@@ -1,5 +1,5 @@
-import Footer from "../../../components/components - globals/Footer";
-import Header from "../../../components/components - globals/Header";
+import Footer from "../../../components/components - globals/components/Footer";
+import Header from "../../../components/components - globals/components/Header";
 import SectionBuscador from "../../../components/components - servicios/components/section - buscador";
 import SectionServicios from "../../../components/components - servicios/components/section - servicios";
 
