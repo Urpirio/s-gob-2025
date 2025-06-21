4a478f66896956ff93768838083fcd0383975de1
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-21 18:11:19 -0400

    Se agrego un funcion para desactivar el menu al detectar que el usario esta navegando

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,27 @@
-import Footer from "../../components/components - globals/Footer"
-import Header from "../../components/components - globals/Header"
-import SectionInputSearch from "../../components/components - Home/components/Section - InputSearch"
-import SectionPreguntas from "../../components/components - Home/components/Section - Preguntas"
+'use client';
+import Footer from "../../components/components - globals/components/Footer";
+import Header from "../../components/components - globals/components/Header";
+import SectionInputSearch from "../../components/components - Home/components/Section - InputSearch";
+import SectionPreguntas from "../../components/components - Home/components/Section - Preguntas";
 import SectionPuntoGob from "../../components/components - Home/components/Section - PuntoGob"
 import SectionSearch from "../../components/components - Home/components/Section - Search"
 import SectionServicios from "../../components/components - Home/components/Section - Servicios"
-import SectionTramites from "../../components/components - Home/components/Section - Tramites"
-import SectionComentarios from "../../components/components - Home/components/Section-comentarios"
+import SectionTramites from "../../components/components - Home/components/Section - Tramites";
+import SectionComentarios from "../../components/components - Home/components/Section-comentarios";
+import { SetMenu } from "../../components/components - globals/components/Header";
+
 
 export default function Home() {
+
+  const Close_Movil_Menu = () => {
+   SetMenu(false);
+  }
+
+
   return (
-    <body>
+    <body onTouchMove={()=>{
+      Close_Movil_Menu();
+    }}>
         <Header/>
         <main>
             <SectionSearch/>
