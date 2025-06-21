2e518b79cba2817d9d86f4239c23d996e284fd71
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-20 23:07:41 -0400

    Se agrego la fase 1 de la responsividad

--- components/components - Home/components/Section-comentarios.jsx
+++ components/components - Home/components/Section-comentarios.jsx
@@ -8,18 +8,17 @@ export default function SectionComentarios() {
             <div className="text-center">
                 <h1 className="text-4xl">“Transformando el servicio contigo”</h1>
             </div>
-           <div className="w-[100%] flex justify-center gap-10">
-            <div className="flex items-center">
+           <div className="w-[100%] flex justify-around items-center lg:justify-center relative gap-10">
+            <div className="flex items-center w-full h-full lg:w-[10%] justify-start absolute lg:relative">
                 <button className="text-6xl text-[#007AFF]"><IoIosArrowBack/></button>
             </div>
-             <div  className="flex justify-start gap-5">
+             <div  className="flex justify-center md:justify-start overflow-auto     scrollbar-hide gap-5">
                 <CardComentarios/>
             </div>
-            <div className="flex items-center" >
+            <div className="flex items-center w-full h-full lg:w-[10%] justify-end absolute lg:relative" >
                 <button className="text-6xl text-[#007AFF]"><IoIosArrowForward/></button>
             </div>
-           </div>
-           
+           </div>     
         </section>
     )
 }
