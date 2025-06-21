5199396c07686939075597cf6fd5fae7e5552aa5
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-20 23:08:00 -0400

    Se agrego la fase 1 de la responsividad

--- components/components - Home/SubComponents/CardGob.jsx
+++ components/components - Home/SubComponents/CardGob.jsx
@@ -10,7 +10,7 @@ const roboto = Roboto({
 export default function CardGob() {
   const CardG = DataCardPuntoGob.map(Data =>{
     return (
-    <article key={Data.key} className={`border border-gray-300 w-[35%] max-w-100 min-w-90 p-5 rounded-xl flex flex-col gap-1 ${roboto.className} shadow-2xs`}>
+    <article key={Data.key} className={`border border-gray-300 w-[35%] max-w-100 min-w-80 p-5 rounded-xl flex flex-col gap-1 ${roboto.className} shadow-2xs`}>
         <header className="flex flex-col gap-2">
             <span className="text-[#0088FF] font-semibold">{Data.NombreGob}</span>
             <Image src={ImgPrueba} className="rounded-md" alt={Data.NombreGob}/>
@@ -27,8 +27,8 @@ export default function CardGob() {
         [&footer>button]:rounded-md
         [&footer>button]:
         ">
-            <button className="bg-[#D9D9D9] text-[#1369CC]">Detalles</button>
-            <button className="bg-[#1369CC] text-white">solicitar servicio</button>
+            <button className="bg-[#D9D9D9] text-sm lg:text-base text-[#1369CC]">Detalles</button>
+            <button className="bg-[#1369CC] text-sm lg:text-base text-white">solicitar servicio</button>
         </footer>
         </article>
 
