acdf243631edb5f7dee3d075a60de1f29dba9f07
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-20 23:07:10 -0400

    Se agrego la fase 1 de la responsividad

--- components/components - Home/components/Section - Preguntas.jsx
+++ components/components - Home/components/Section - Preguntas.jsx
@@ -9,15 +9,15 @@ const albertsans = Albert_Sans({
 
 export default function SectionPreguntas() {
   return (
-    <section className={`flex flex-col p-5 ${albertsans.className}`}>
-        <div className="flex justify-center p-5">
-            <h1 className="text-5xl">Preguntas frecuentes</h1>
+    <section className={`flex flex-col px-5 py-10 ${albertsans.className}`}>
+        <div className="flex justify-center py-5">
+            <h1 className=" text-3xl text-center lg:text-5xl">Preguntas frecuentes</h1>
         </div>
-        <section className="flex justify-around">
-            <List className="w-[45%] flex flex-col justify-center gap-1">
+        <section className="flex flex-wrap w-[100%] justify-center items-center md:justify-around ">
+            <List className="w-[45%] flex flex-col justify-center gap-1 min-w-80">
                 <Preguntas/>
             </List>
-            <div className="w-[45%] ">
+            <div className="w-[45%] min-w-90 flex justify-center">
                 <EscribirPreguntas/>
             </div>
         </section>
