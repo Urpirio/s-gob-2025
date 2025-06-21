f6b550dc8b07fcd831ab6ef4633896b58adc0068
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-20 23:07:26 -0400

    Se agrego la fase 1 de la responsividad

--- components/components - Home/components/Section - PuntoGob.jsx
+++ components/components - Home/components/Section - PuntoGob.jsx
@@ -8,11 +8,11 @@ const Albert_sans = Albert_Sans({
 export default function SectionPuntoGob() {
   return (
     <section className="pt-10 pb-10">
-        <div className="flex flex-col text-center gap-5 pt-5 pb-10">
-            <h1 className={`${Albert_sans.className} text-6xl font-semibold`}>Instituciones afiliadas</h1>
-            <span className={`${Albert_sans.className} font-extralight text-4xl`}>Transformando la gestión pública contigo.</span>
+        <div className="flex flex-col text-center gap-5 pt-5 pb-10 px-1 lg:px-0">
+            <h1 className={`${Albert_sans.className} text-4xl lg:text-6xl font-semibold`}>Instituciones afiliadas</h1>
+            <span className={`${Albert_sans.className} font-extralight text-2xl lg:text-4xl`}>Transformando la gestión pública contigo.</span>
         </div>
-        <div className="flex flex-wrap justify-around">
+        <div className="flex flex-wrap justify-center xxl:justify-around p-2 gap-5 xxl:gap-0">
             <CardGob/>
         </div>
     </section>
