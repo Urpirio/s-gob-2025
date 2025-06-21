1136eeab4023d5db2503bddf7c99e26f2fcb6574
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-20 23:07:30 -0400

    Se agrego la fase 1 de la responsividad

--- components/components - Home/components/Section - Search.jsx
+++ components/components - Home/components/Section - Search.jsx
@@ -2,13 +2,13 @@
 export default function SectionSearch() {
   return (
     <section className="py-10 flex flex-col items-center justify-center gap-5">
-      <div className="text-center w-[40%]">
-        <h1 className="text-6xl">
+      <div className="text-center w-[40%] min-w-90">
+        <h1 className=" text-4xl lg:text-6xl">
           Hola, ¿Que vamos 
           a <i className="text-[#0088FF] font-">gestionar</i> hoy?
         </h1>
       </div>
-      <div className="text-center w-[45%]">
+      <div className="text-center w-[45%] min-w-90">
         <h2 className="text-3xl font-extralight">Gestiona tus tramites con mayor precision, mejor enfoque y sin perder tiempo.</h2>
       </div>
     </section>
