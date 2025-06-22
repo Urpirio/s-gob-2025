27441d6866361f8b543a37aa8ff12741833f4135
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-21 21:37:50 -0400

    Se agrego la funcion que desactiva el menu cuando el mismo esta deplegado en movil y el usuario quiere navegar

--- src/app/AgendarCita/page.jsx
+++ src/app/AgendarCita/page.jsx
@@ -1,14 +1,14 @@
-'use client'
+'use client';
 import SectionFormulario from "../../../components/components - AgendarCita/components/Section - formulario";
 import Footer from "../../../components/components - globals/components/Footer";
 import Header from "../../../components/components - globals/components/Header";
-
+import { SetMenu } from "../../../components/components - globals/components/Header";
 
 export default function page() {
 
 
   return (
-    <body>
+    <body onTouchMove={()=>{SetMenu()}}>
         <Header/>
         <main>
             <SectionFormulario/>
