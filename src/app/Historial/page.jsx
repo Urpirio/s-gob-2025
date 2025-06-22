cdb9ea0ddd5e51cdf1ecb63e0891520bcfcb5cd2
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-21 21:38:06 -0400

    Se refactorizo el codigo

--- src/app/Historial/page.jsx
+++ src/app/Historial/page.jsx
@@ -1,19 +1,23 @@
-
+'use client';
+import Footer from '../../../components/components - globals/components/Footer';
 import Header from '../../../components/components - globals/components/Header';
 import SectionTitleHistorial from '../../../components/components - Historial/components/Section - TitleHistorial';
 import SectionTableHistorial from '../../../components/components - Historial/components/Section - TableHistorial';
+import { SetMenu } from '../../../components/components - globals/components/Header';
 
+export default function page() {
 
-const HistoricalData = () => {
+  
+
   return (
-    <body>
-        <Header/>
-        <main>
-          <SectionTitleHistorial/>
-          <SectionTableHistorial/>
-        </main>
+    <body onTouchMove={()=>{SetMenu()}}>
+      <Header/>
+      <main>
+        <SectionTitleHistorial/>
+        <SectionTableHistorial/>
+      </main>
+      <Footer/>
     </body>
-  );
-};
+  )
+}
 
-export default HistoricalData;
