e16e4168c294863759b1dee0f4992c12f35df3aa
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-21 21:38:37 -0400

    Se agrego la funcion que desactiva el menu cuando el mismo esta deplegado en movil y el usuario quiere navegar

--- src/app/Perfil/page.jsx
+++ src/app/Perfil/page.jsx
@@ -1,8 +1,10 @@
+'use client';
 import Footer from "../../../components/components - globals/components/Footer";
 import Header from "../../../components/components - globals/components/Header";
 import SectionPerfil from "../../../components/components - perfil/Components/Section - Perfil";
 import SectionTitle from "../../../components/components - perfil/Components/Section - Title";
 import { Albert_Sans } from "next/font/google";
+import { SetMenu } from "../../../components/components - globals/components/Header";
 
 const albertsans = Albert_Sans({
     subsets: ['latin-ext']
@@ -10,7 +12,7 @@ const albertsans = Albert_Sans({
 
 export default function page() {
   return (
-    <body>
+    <body onTouchMove={()=>{SetMenu()}}>
         <Header/>
         <main className={`${albertsans.className}`}>
             <SectionTitle/>
