ee2b5ece6b53d38d7dd27f361c55861a49dabb8d
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-06-21 21:38:29 -0400

    Se mejoro el estilo movile

--- src/app/Login/page.jsx
+++ src/app/Login/page.jsx
@@ -1,18 +1,23 @@
+'use client';
 import Header from "../../../components/components - globals/components/Header";
 import SectionLogin from "../../../components/components - Login/components/Section - Login";
 import ImgRegistro from '@/../public/img - Login/Sign up-rafiki 1.png';
 import Image from "next/image";
+import Footer from "../../../components/components - globals/components/Footer";
+// import { SetMenu } from "../../../components/components - globals/components/Header";
 
 export default function Login() {
+
   return (
-    <body>
-      {/* <Header/> */}
+    <body onTouchMove={()=>{SetMenu()}} >
+      <Header/>
       <main className="flex justify-around items-center h-[100vh]">
         <SectionLogin/>
          <section>
-            <Image src={ImgRegistro} alt=""/>
+            <Image className="hidden lg:block" src={ImgRegistro} alt=""/>
         </section>
       </main>
+      <Footer/>
     </body>
   )
 }
