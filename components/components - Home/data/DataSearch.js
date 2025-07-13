688eebe7bf58086d97c2d8c583db1041653d4713
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-13 13:53:28 -0400

    Se agrego un array de datos para agregar el sistema de busqueda

---
+++ components/components - Home/data/DataSearch.js
@@ -0,0 +1,21 @@
+
+export const DataSearh = [
+    {
+        Contenido: '¿Como sacar mi cédula?',
+    },
+    {
+        Contenido: '¿Como sacar un papel de buena conducta?',
+    },
+    {
+        Contenido: '¿Como legalizar unos documentos?',
+    },
+    {
+        Contenido: '¿Como renovar mi pasaporte?',
+    },
+    {
+        Contenido: '¿Como sacar mi acta de nacimiento?',
+    },
+    {
+        Contenido: '¿Como renovar mi cédula?',
+    }
+]
