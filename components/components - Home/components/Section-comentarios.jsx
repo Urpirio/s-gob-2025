db085eb32688fb6435adf4bacaf64892731d48d5
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-13 09:15:14 -0400

    Se refactorizo el codigo y se monto un libreria

--- components/components - Home/components/Section-comentarios.jsx
+++ components/components - Home/components/Section-comentarios.jsx
@@ -2,32 +2,34 @@
 import { IoIosArrowForward } from "react-icons/io";
 import { IoIosArrowBack } from "react-icons/io";
 import CardComentarios from "../SubComponents/Card-comentarios";
-import { useNavCard } from "../../../hook/useNavCard";
+import { Carousel } from 'primereact/carousel';
+import { DataCardComentarios } from "../data/DataCardComentarios";
 
 export default function SectionComentarios() {
 
-    const {Navigate} = useNavCard();
+     const responsiveOptions = [
+        {
+            breakpoint: '667px',
+            numVisible: 1,
+        },
+     ]
 
     return(
         <section className="py-10 flex flex-col gap-10">
             <div className="text-center">
                 <h1 className="text-4xl">“Transformando el servicio contigo”</h1>
             </div>
-           <div className="w-[100%] flex justify-around items-center lg:justify-center relative  lg:gap-10 ">
-            <div className="flex  items-center w-full h-1/2 lg:w-[10%] justify-start ">
-                <button onClick={()=>{Navigate({Right: false,Left: true,IdContainer: 'CardContainer'})}} className=" absolute text-6xl text-[#007AFF]">
-                    <IoIosArrowBack/>
-                </button>
+             <div  id="CardContainer" className="flex  w-full md:px-10 justify-center" style={{scrollbarWidth:'none'}}>
+                <Carousel 
+                value={DataCardComentarios} 
+                numVisible={3} className=" w-[100%]   " 
+                autoplayInterval={3000}
+                responsiveOptions={responsiveOptions}
+                containerClassName="flex gap-10"
+                nextIcon={<IoIosArrowForward  className="text-4xl text-primary"/>}
+                prevIcon={<IoIosArrowBack className="text-4xl text-primary"/>}
+                itemTemplate={(D)=><CardComentarios Data={D}/>}/>
             </div>
-             <div  id="CardContainer" className="flex min-w-90 px-10 lg:px-0 justify-start  lg:justify-end   overflow-x-scroll    scrollbar-hide gap-5">
-                <CardComentarios/>
-            </div>
-            <div  className=" flex  items-center w-full h-1/2 lg:w-[10%] justify-end " >
-                <button onClick={()=>{Navigate({Right: true,Left: false,IdContainer: 'CardContainer'})}} className=" absolute text-6xl text-[#007AFF]">
-                    <IoIosArrowForward/>
-                </button>
-            </div>
-           </div>     
         </section>
     )
 }
