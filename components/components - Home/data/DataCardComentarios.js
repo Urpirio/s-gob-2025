293ec08ad85594a4a600659bdb23b4520ac1dc13
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-13 09:14:57 -0400

    Se agregaron dos objetos con datos para simular los comentarios

--- components/components - Home/data/DataCardComentarios.js
+++ components/components - Home/data/DataCardComentarios.js
@@ -33,4 +33,27 @@ export const DataCardComentarios = [
         ahora no puedo dejar de pensar
         en eso.`,
     },
+    {
+        id: 'CardC2',
+        key:2,
+        Star: 5,
+        img: 'https://i.pinimg.com/736x/c8/63/66/c863667f3c81576ec3b3f029fe1a4087.jpg',
+        name: 'Juan Hernandez',
+        UserName: '@juan.hernandez_',
+        Comment: `Desde que empece a gestionar mis 
+        citas desde Gestion sin limites todo
+        es mas facil.`,
+    },
+    {
+        id: 'CardC3',
+        key:3,
+        Star: 5,
+        img: 'https://i.pinimg.com/736x/76/c2/e4/76c2e44e5eec4554a6dbb73bef0a6169.jpg',
+        name: 'Elías Navarro',
+        UserName: 'elias.navv',
+        Comment: `Hoy leí que las abejas pueden
+        reconocer rostros humanos… y
+        ahora no puedo dejar de pensar
+        en eso.`,
+    },
 ]
