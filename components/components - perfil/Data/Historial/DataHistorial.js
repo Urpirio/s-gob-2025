dac944cb35ce53d318958a168258aa3405ab43c4
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-13 23:34:10 -0400

    Se agrego un array de datos para el historial d

---
+++ components/components - perfil/Data/Historial/DataHistorial.js
@@ -0,0 +1,62 @@
+export const DataHistorial = [
+    {
+        Servicios:{
+            Nombre: 'Cambio de cédula',
+            Institucion: 'Junta Central Electoral'
+            },
+        Fecha: {
+                hora:'10:30 AM',
+                Dia:'Miércoles 15 de Mayo'
+            },
+        Estado: 'Procesado',
+        Tickets:'Ver ticket',
+    },
+    {
+        Servicios:{
+            Nombre: 'Cambio de cédula',
+            Institucion: 'Junta Central Electoral'
+            },
+        Fecha: {
+                hora:'10:30 AM',
+                Dia:'Miércoles 15 de Mayo'
+            },
+        Estado: 'Activo',
+        Tickets:'Ver ticket',
+    },
+    {
+        Servicios:{
+            Nombre: 'Cambio de cédula',
+            Institucion: 'Junta Central Electoral'
+            },
+        Fecha: {
+                hora:'10:30 AM',
+                Dia:'Miércoles 15 de Mayo'
+            },
+        Estado: 'Activo',
+        Tickets:'Ver ticket',
+    },
+    {
+        Servicios:{
+            Nombre: 'Cambio de cédula',
+            Institucion: 'Junta Central Electoral'
+            },
+        Fecha: {
+                hora:'10:30 AM',
+                Dia:'Miércoles 15 de Mayo'
+            },
+        Estado: 'Fallido',
+        Tickets:'Ver ticket',
+    },
+    {
+        Servicios:{
+            Nombre: 'Cambio de cédula',
+            Institucion: 'Junta Central Electoral'
+            },
+        Fecha: {
+                hora:'10:30 AM',
+                Dia:'Miércoles 15 de Mayo'
+            },
+        Estado: 'Activo',
+        Tickets:'Ver ticket',
+    }
+]
