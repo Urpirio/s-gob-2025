c1865c3c2d118b2ea76544a2456616ed46718fd3
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-13 23:36:16 -0400

    Se agrego el layout del historial y las diversas tarjetas

---
+++ src/app/Perfil/layout.jsx
@@ -0,0 +1,16 @@
+import Footer from "../../../components/components - globals/components/Footer";
+import Header from "../../../components/components - globals/components/Header";
+import SectionNavegacion from "../../../components/components - perfil/Components/Perfil/Section- Navegacion";
+
+export default function layout({children}) {
+  return (
+    <body>
+        <Header/>
+        <main className="flex flex-col items-center w-full">
+            <SectionNavegacion/>
+            {children}
+        </main>
+        <Footer/>
+    </body>
+  )
+}
