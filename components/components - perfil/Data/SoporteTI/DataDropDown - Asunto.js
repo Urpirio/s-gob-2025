d8135197c594a9279e31d845161f0cc4a8e61506
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-14 16:00:10 -0400

    Se agrego un array de datos de formualrio de soporte eso para el dropdown

---
+++ components/components - perfil/Data/SoporteTI/DataDropDown - Asunto.js
@@ -0,0 +1,15 @@
+
+export const DataDropDown_Asunto = [
+    {
+        Asunto: 'Opcion 1'
+    },
+    {
+        Asunto: 'Opcion 2'
+    },
+    {
+        Asunto: 'Opcion 3'
+    },
+    {
+        Asunto: 'Opcion 4'
+    }
+]
