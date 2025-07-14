6af92f62f1565f05b7b1a206f9bdbcc3e21b9567
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-13 23:35:57 -0400

    Se agrego el historial dentro de la carpeta de perfil

---
+++ src/app/Perfil/Historial/page.jsx
@@ -0,0 +1,11 @@
+import SectionTableHistorial from "../../../../components/components - perfil/Components/HIstorial/Section - TableHistorial";
+
+export default function page() {
+
+  return (
+      <main>
+        <SectionTableHistorial/>
+      </main>
+  )
+}
+
