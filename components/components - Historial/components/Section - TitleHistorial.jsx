0f3ead9cfe9f75ec0597ccf4c690a576d8c28cfe
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-13 23:31:45 -0400

    Se elimino

--- components/components - Historial/components/Section - TitleHistorial.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function SectionTitleHistorial() {
-  return (
-    <section className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Historial</h1>
-    </section>
-  )
-}
