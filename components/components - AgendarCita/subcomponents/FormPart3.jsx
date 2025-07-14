2e2d5724191e39305a71c3471812f142af0b6ec8
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-13 23:30:46 -0400

    Se hicieron mejoras en el estilo

--- components/components - AgendarCita/subcomponents/FormPart3.jsx
+++ components/components - AgendarCita/subcomponents/FormPart3.jsx
@@ -1,42 +1,44 @@
-import React from 'react'
+'use client';
 
-export default function FormPart3() {
+export default function FormPart3({InputsForm,DropdownPGob,DropdownTramite}) {
+
+
   return (
-    <div className='hidden md:flex flex-col items-center justify-between border   border-gray-400 w-[30%] min-w-90 py-10 px-5 rounded-2xl'>
-      <div className='flex gap-3  items-center w-[100%]'>
+    <div className='hidden md:flex flex-col items-center justify-between border gap-1  border-gray-400 w-[30%] min-w-90 py-10 px-5 rounded-2xl'>
+      <div className='flex  flex-col w-[100%]'>
         <h1 className='text-4xl text-[#0088FF] font-semibold'>Datos de tu cita</h1>
       </div>
-      <div className='flex gap-3  items-center w-[100%]'>
+      <div className='flex  flex-col w-[100%]'>
         <h1 className='text-xl text-[#0088FF] font-semibold'>Tramite:</h1>
-        <span className='text-xl text-gray-500'>Por defecto</span>
+        <span className='text-md text-gray-500'>{DropdownTramite ? DropdownTramite :'No disponible' }</span>
       </div>
-      <div className='flex gap-3  items-center w-[100%]'>
+      <div className='flex  flex-col w-[100%]'>
         <h1 className='text-xl text-[#0088FF] font-semibold'>Institucion:</h1>
-        <span className='text-xl text-gray-500'>Por defecto</span>
+        <span className='text-md text-gray-500'>{DropdownPGob ? DropdownPGob : 'No disponible'}</span>
       </div>
-      <div className='flex gap-3  items-center w-[100%]'>
+      <div className='flex  flex-col w-[100%]'>
         <h1 className='text-xl text-[#0088FF] font-semibold'>Nombres:</h1>
-        <span className='text-xl text-gray-500'>John Doe</span>
+        <span className='text-md text-gray-500'>{InputsForm?.Nombres}</span>
       </div>
-      <div className='flex gap-3  items-center w-[100%]'>
+      <div className='flex  flex-col w-[100%]'>
         <h1 className='text-xl text-[#0088FF] font-semibold'>Apellidos:</h1>
-        <span className='text-xl text-gray-500'>Genez Suarez</span>
+        <span className='text-md text-gray-500'>{InputsForm?.Apellidos}</span>
       </div>
-      <div className='flex gap-3  items-center w-[100%]'>
+      <div className='flex  flex-col w-[100%]'>
         <h1 className='text-xl text-[#0088FF] font-semibold'>Telefono:</h1>
-        <span className='text-xl text-gray-500'>000-000-0000</span>
+        <span className='text-md text-gray-500'>{InputsForm?.Telefono}</span>
       </div>
-      <div className='flex gap-3  items-center w-[100%]'>
+      <div className='flex  flex-col w-[100%]'>
         <h1 className='text-xl text-[#0088FF] font-semibold'>Correo electronico:</h1>
-        <span className='text-xl text-gray-500'>johndoe@gmail.com</span>
+        <span className='text-md text-gray-500'>{InputsForm?.Correo}</span>
       </div>
-      <div className='flex gap-3  items-center w-[100%]'>
+      <div className='flex flex-col  w-[100%]'>
         <h1 className='text-xl text-[#0088FF] font-semibold'>Cedula:</h1>
-        <span className='text-xl text-gray-500'>000-0000000-0</span>
+        <span className='text-md text-gray-500'>{InputsForm?.Cedula}</span>
       </div>
-      <div className='flex gap-3  items-center w-[100%]'>
+      <div className='flex  flex-col w-[100%]'>
         <h1 className='text-xl text-[#0088FF] font-semibold'>Direccion:</h1>
-        <span className='text-xl text-gray-500'>Calle ej. #1, Provincia, Pais. </span>
+        <span className='text-md text-gray-500'>{InputsForm?.Direccion}</span>
       </div>
       <div>
         <button className=' px-5 py-2 bg-[#0088FF] text-white rounded-md'>Confirmar cita</button>
