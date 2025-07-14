16c800b1be2f01fc1575efa0e31b9f161cb11eca
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-13 23:28:57 -0400

    Se hicieron pequeñas modificaciones a los componentes

--- components/components - AgendarCita/components/Section - formulario.jsx
+++ components/components - AgendarCita/components/Section - formulario.jsx
@@ -1,18 +1,36 @@
+'use client';
 import FormPart1 from "../subcomponents/FormPart1";
 import FormPart2 from "../subcomponents/FormPart2";
 import FormPart3 from "../subcomponents/FormPart3";
 import { Albert_Sans } from "next/font/google";
+import { useState } from "react";
 
 const albert_Sans = Albert_Sans({
   subsets: ['latin-ext']
 });
 
 export default function SectionFormulario() {
+
+
+  const [InputValues,setInputValues] = useState({
+    Apellidos: String(),
+    Telefono: String(),
+    Nombres: String(),
+    Correo:String(),
+    Cedula:String(),
+    Direccion:String(),
+  });
+
   return (
     <section className={`flex justify-around md:p-10 py-10 md:py-10 flex-wrap ${albert_Sans.className}`}>
-            <FormPart2/>
-            <FormPart1/>
-            <FormPart3/>
+            <FormPart2
+            InputValues={InputValues} 
+            setInputValues={setInputValues}/>
+            <FormPart1 
+            InputValues={InputValues} 
+            setInputValues={setInputValues}/>
+            <FormPart3 
+            InputsForm={InputValues}/>
     </section>
   )
 };
