21bdd269cb540c9c67e73ee43260a7723dea5193
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-13 23:36:34 -0400

    Se optmizo el codigo

--- src/app/Perfil/page.jsx
+++ src/app/Perfil/page.jsx
@@ -1,24 +1,15 @@
 'use client';
-import Footer from "../../../components/components - globals/components/Footer";
-import Header from "../../../components/components - globals/components/Header";
-import SectionPerfil from "../../../components/components - perfil/Components/Section - Perfil";
-import SectionTitle from "../../../components/components - perfil/Components/Section - Title";
+import SectionPerfil from "../../../components/components - perfil/Components/Perfil/Section - Perfil";
 import { Albert_Sans } from "next/font/google";
-import { SetMenu } from "../../../components/components - globals/components/Header";
 
 const albertsans = Albert_Sans({
     subsets: ['latin-ext']
-})
+});
 
 export default function page() {
   return (
-    <body onTouchMove={()=>{SetMenu()}}>
-        <Header/>
-        <main className={`${albertsans.className}`}>
-            <SectionTitle/>
+        <main className={`${albertsans.className} w-[70%]`}>
             <SectionPerfil/>
         </main>
-        <Footer/>
-    </body>
   )
-}
+};
