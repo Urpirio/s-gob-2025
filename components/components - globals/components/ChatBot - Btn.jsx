28815b236338c7315972b45d57c8b055465d7307
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-21 12:20:51 -0400

    Se agrego un btn

---
+++ components/components - globals/components/ChatBot - Btn.jsx
@@ -0,0 +1,11 @@
+
+import { DeployChatBotContainer } from "./ChatBotContainer";
+import { BsChatDots } from "react-icons/bs";
+
+export default function ChatBotBtn() {
+  return (
+    <button className="fixed border z-50 w-full">
+        <BsChatDots/>
+    </button>
+  )
+}
