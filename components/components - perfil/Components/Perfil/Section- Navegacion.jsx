ed28f7e466a03d9c9d7e974ad26368298fd35218
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-07-21 07:52:38 -0400

    Se hicieron mejoras en la responsividad

--- components/components - perfil/Components/Perfil/Section- Navegacion.jsx
+++ components/components - perfil/Components/Perfil/Section- Navegacion.jsx
@@ -12,23 +12,23 @@ export default function SectionNavegacion() {
 
 
   return (
-    <section className="flex justify-center py-2 w-full">
-        <nav className="flex items-center gap-2 w-[60%] justify-between p-1 rounded-md border border-gray-100 bg-[#f8f9fa]">
+    <section className="flex justify-center py-2 w-full ">
+        <nav className="flex flex-wrap md:flex-nowrap items-center gap-2 w-[90%] md:w-[60%] justify-between p-1 rounded-md border border-gray-100 bg-[#f8f9fa]">
           <Link href={'/Perfil'}
-          className={`flex items-center justify-center gap-2  w-1/3 p-2 rounded-md ${Pathname === '/Perfil' ? 'bg-primary text-white border' : ''}`}>
+          className={`flex items-center justify-center gap-2  md:w-1/3 w-full  p-2 rounded-md ${Pathname === '/Perfil' ? 'bg-primary text-white border' : ''}`}>
           <FiUsers/>
           <span>Perfil</span>
           </Link>
 
           <Link href={'/Perfil/Historial'}
-          className={`flex items-center justify-center gap-2  w-1/3 p-2 rounded-md ${Pathname === '/Perfil/Historial' ? 'bg-primary text-white border' : ''}`}
+          className={`flex items-center justify-center gap-2  md:w-1/3 w-full p-2 rounded-md ${Pathname === '/Perfil/Historial' ? 'bg-primary text-white border' : ''}`}
           >
           <GoFileDirectory/>
           <span>Historial</span>
           </Link>
 
           <Link href={'/Perfil/SoporteTecnico'}
-          className={`flex items-center justify-center gap-2  w-1/3 p-2 rounded-md ${Pathname === '/Perfil/SoporteTecnico' ? 'bg-primary text-white border' : ''}`}>
+          className={`flex items-center justify-center gap-2  md:w-1/3 w-full  p-2 rounded-md ${Pathname === '/Perfil/SoporteTecnico' ? 'bg-primary text-white border' : ''}`}>
           <FaRegQuestionCircle/>
           <span>Soporte Tecnico</span>
           </Link>
