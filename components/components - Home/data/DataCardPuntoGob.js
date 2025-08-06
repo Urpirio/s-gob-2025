60aff6442ca0fc359c71a274b5b0e8f4cf4f96aa
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-06 10:58:02 -0400

    Actualizar los nombres de las tarjetas de Punto Gob y habilitar la cuarta entrada
    Se cambió el campo 'NombreGob' para todas las entradas a 'Punto Gob - Prueba X' y se descomentó la cuarta tarjeta en DataCardPuntoGob para fines de prueba o demostración.

--- components/components - Home/data/DataCardPuntoGob.js
+++ components/components - Home/data/DataCardPuntoGob.js
@@ -1,7 +1,7 @@
 export const DataCardPuntoGob = [
     {
         key: 1,
-        NombreGob: 'Punto Gob - Sambil',
+        NombreGob: 'Punto Gob - Prueba 1',
         Distancia: '1.8km de distancia - Sambil, Santo Domingo',
         Descripcion: `Encuentra todos los servicios del Gobierno Dominicano en 
         un solo Portal. Acceso rápido, seguro y digital a trámites
@@ -10,7 +10,7 @@ export const DataCardPuntoGob = [
     },
     {
         key: 2,
-        NombreGob: 'Punto Gob - Sambil',
+        NombreGob: 'Punto Gob - Prueba 2',
         Distancia: '1.8km de distancia - Sambil, Santo Domingo',
         Descripcion: `Encuentra todos los servicios del Gobierno Dominicano en 
         un solo Portal. Acceso rápido, seguro y digital a trámites
@@ -19,19 +19,19 @@ export const DataCardPuntoGob = [
     },
     {
         key: 3,
-        NombreGob: 'Punto Gob - Sambil',
+        NombreGob: 'Punto Gob - Prueba 3',
         Distancia: '1.8km de distancia - Sambil, Santo Domingo',
         Descripcion: `Encuentra todos los servicios del Gobierno Dominicano en 
         un solo Portal. Acceso rápido, seguro y digital a trámites
         y servicios para todos los ciudadanos.`,
         
     },
-    // {
-    //     key: 4,
-    //     NombreGob: 'Punto Gob - Sambil',
-    //     Distancia: '1.8km de distancia - Sambil, Santo Domingo',
-    //     Descripcion: `Encuentra todos los servicios del Gobierno Dominicano en 
-    //     un solo Portal. Acceso rápido, seguro y digital a trámites
-    //     y servicios para todos los ciudadanos.`,
-    // }
+    {
+        key: 4,
+        NombreGob: 'Punto Gob - Prueba 4',
+        Distancia: '1.8km de distancia - Sambil, Santo Domingo',
+        Descripcion: `Encuentra todos los servicios del Gobierno Dominicano en 
+        un solo Portal. Acceso rápido, seguro y digital a trámites
+        y servicios para todos los ciudadanos.`,
+    }
 ];
