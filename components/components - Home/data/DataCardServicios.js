a2a9ea7a1f7c0bec6feba01e1f529325016a99e6
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-06 10:25:29 -0400

    Se agregaron los servicios de matriz DataCardData
    Presenta la matriz DataCardServicios con datos de tarjetas de servicio que incluyen títulos y descripciones de varias ubicaciones de Punto Gob.

---
+++ components/components - Home/data/DataCardServicios.js
@@ -0,0 +1,45 @@
+export const DataCardServicios = [
+  {
+    titulo: "Punto Gob - Sambil",
+    descripcion: ` Ciudadano, moderno, eficaz, eficiente, productivo,
+           participativo y dinámico, incorporando el uso de
+            los recursos de información para la prestación de los servicios`,
+  },
+  {
+    titulo: "Punto Gob -  San Cristobal",
+    descripcion: ` Ciudadano, moderno, eficaz, eficiente, productivo,
+           participativo y dinámico, incorporando el uso de
+            los recursos de información para la prestación de los servicios`,
+  },
+  {
+    titulo: "Punto Gob - Santiago ",
+    descripcion: ` Ciudadano, moderno, eficaz, eficiente, productivo,
+           participativo y dinámico, incorporando el uso de
+            los recursos de información para la prestación de los servicios`,
+  },
+  {
+    titulo: "Punto Gob - Megacentro",
+    descripcion: ` Ciudadano, moderno, eficaz, eficiente, productivo,
+           participativo y dinámico, incorporando el uso de
+            los recursos de información para la prestación de los servicios`,
+  },
+  {
+    titulo: "Punto Gob - Prueba 2",
+    descripcion: ` Ciudadano, moderno, eficaz, eficiente, productivo,
+           participativo y dinámico, incorporando el uso de
+            los recursos de información para la prestación de los servicios`,
+  },
+  {
+    titulo: "Punto Gob - Prueba 3",
+    descripcion: ` Ciudadano, moderno, eficaz, eficiente, productivo,
+           participativo y dinámico, incorporando el uso de
+            los recursos de información para la prestación de los servicios`,
+  },
+
+  {
+    titulo: "Punto Gob - Prueba 4",
+    descripcion: ` Ciudadano, moderno, eficaz, eficiente, productivo,
+           participativo y dinámico, incorporando el uso de
+            los recursos de información para la prestación de los servicios`,
+  },
+];
