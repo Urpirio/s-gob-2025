82d07d682d1c79e0f49ab538ba4695625adf5514
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-06 10:59:40 -0400

    se agrego propiedad de clave única a los elementos de DataCardServicios
    Cada objeto del array DataCardServicios ahora incluye una propiedad "key" única. Este cambio facilita una representación e identificación más fiables de los elementos en los componentes de React.

--- components/components - Home/data/DataCardServicios.js
+++ components/components - Home/data/DataCardServicios.js
@@ -1,35 +1,41 @@
 export const DataCardServicios = [
   {
+    key: 1,
     titulo: "Punto Gob - Sambil",
     descripcion: ` Ciudadano, moderno, eficaz, eficiente, productivo,
            participativo y dinámico, incorporando el uso de
             los recursos de información para la prestación de los servicios`,
   },
   {
+    key: 2,
     titulo: "Punto Gob -  San Cristobal",
     descripcion: ` Ciudadano, moderno, eficaz, eficiente, productivo,
            participativo y dinámico, incorporando el uso de
             los recursos de información para la prestación de los servicios`,
   },
   {
+    key: 3,
     titulo: "Punto Gob - Santiago ",
     descripcion: ` Ciudadano, moderno, eficaz, eficiente, productivo,
            participativo y dinámico, incorporando el uso de
             los recursos de información para la prestación de los servicios`,
   },
   {
+    key: 4,
     titulo: "Punto Gob - Megacentro",
     descripcion: ` Ciudadano, moderno, eficaz, eficiente, productivo,
            participativo y dinámico, incorporando el uso de
             los recursos de información para la prestación de los servicios`,
   },
   {
+    key: 5,
     titulo: "Punto Gob - Prueba 2",
     descripcion: ` Ciudadano, moderno, eficaz, eficiente, productivo,
            participativo y dinámico, incorporando el uso de
             los recursos de información para la prestación de los servicios`,
   },
   {
+    key: 6,
     titulo: "Punto Gob - Prueba 3",
     descripcion: ` Ciudadano, moderno, eficaz, eficiente, productivo,
            participativo y dinámico, incorporando el uso de
@@ -37,6 +43,7 @@ export const DataCardServicios = [
   },
 
   {
+    key: 7,
     titulo: "Punto Gob - Prueba 4",
     descripcion: ` Ciudadano, moderno, eficaz, eficiente, productivo,
            participativo y dinámico, incorporando el uso de
