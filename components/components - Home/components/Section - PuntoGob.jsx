44b361a247f041d4fb8a4b49a048fcdaaf1ff316
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-06 10:55:58 -0400

    Reemplace la lista desplazable de CardGob con Carrusel
    Se actualizó SectionPuntoGob para usar el componente Carrusel de Primereact para mostrar las instituciones afiliadas. Las tarjetas ahora se renderizan dinámicamente desde DataCardPuntoGob, lo que mejora el diseño y la experiencia del usuario con reproducción automática y navegación.

--- components/components - Home/components/Section - PuntoGob.jsx
+++ components/components - Home/components/Section - PuntoGob.jsx
@@ -1,20 +1,47 @@
 import { Albert_Sans } from "next/font/google";
 import CardGob from "../SubComponents/CardGob";
+import { Carousel } from "primereact/carousel";
+import { DataCardPuntoGob } from "../data/DataCardPuntoGob";
 
 const Albert_sans = Albert_Sans({
-  subsets: ['latin-ext']
+  subsets: ["latin-ext"],
 });
 
 export default function SectionPuntoGob() {
   return (
     <section className="pt-10 pb-10">
-        <div className="flex flex-col text-center gap-5 pt-5 pb-10 px-1 lg:px-0">
-            <h1 className={`${Albert_sans.className} text-4xl lg:text-6xl font-semibold`}>Instituciones afiliadas</h1>
-            <span className={`${Albert_sans.className} font-extralight text-2xl lg:text-4xl`}>Transformando la gestión pública contigo.</span>
-        </div>
-        <div className="flex overflow-x-scroll  justify-start lg:justify-around p-2 gap-5 xxl:gap-0">
-            <CardGob/>
-        </div>
+      <div className="flex flex-col text-center gap-5 pt-5 pb-10 px-1 lg:px-0">
+        <h1
+          className={`${Albert_sans.className} text-4xl lg:text-6xl font-semibold`}
+        >
+          Instituciones afiliadas
+        </h1>
+        <span
+          className={`${Albert_sans.className} font-extralight text-2xl lg:text-4xl`}
+        >
+          Transformando la gestión pública contigo.
+        </span>
+      </div>
+      <div className="flex w-full justify-center  p-2 gap-5 xxl:gap-0">
+        <Carousel
+          className="w-[80%]"
+          value={DataCardPuntoGob}
+          prevIcon={() => {}}
+          nextIcon={() => {}}
+          numVisible={3}
+          autoplayInterval={5000}
+          itemTemplate={(D) => {
+            return (
+              <CardGob
+                key={D?.key}
+                NombreGob={D?.NombreGob}
+                Distancia={D?.Distancia}
+                Descripcion={D?.Descripcion}
+              />
+            );
+          }}
+        />
+      </div>
     </section>
-  )
+  );
 }
