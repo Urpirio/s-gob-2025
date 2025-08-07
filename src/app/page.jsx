73e8793fc01763d34c25246c369e227917951c75
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-07 11:24:09 -0400

    Se organizo el codigo tse agrego nueva seccion

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,33 +1,37 @@
-'use client';
+"use client";
 import Footer from "../../components/components - globals/components/Footer";
 import Header from "../../components/components - globals/components/Header";
 import SectionInputSearch from "../../components/components - Home/components/Section - InputSearch";
 import SectionPreguntas from "../../components/components - Home/components/Section - Preguntas";
-import SectionPuntoGob from "../../components/components - Home/components/Section - PuntoGob"
-import SectionSearch from "../../components/components - Home/components/Section - Search"
-import SectionServicios from "../../components/components - Home/components/Section - Servicios"
+import SectionPuntoGob from "../../components/components - Home/components/Section - PuntoGob";
+import SectionSearch from "../../components/components - Home/components/Section - Search";
+import SectionServicios from "../../components/components - Home/components/Section - Servicios";
 import SectionTramites from "../../components/components - Home/components/Section - Tramites";
 import SectionComentarios from "../../components/components - Home/components/Section-comentarios";
 import { SetMenu } from "../../components/components - globals/components/Header";
 import ChatBotContainer from "../../components/components - globals/components/ChatBotContainer";
+import Section_MapGoogle from "../../components/components - Home/components/Section - MapGoogle";
 
 export default function Home() {
-
-
   return (
-    <body  onTouchMove={()=>{SetMenu()}}>
-        <Header/>
-        <main>
-            <SectionSearch/>
-            <SectionInputSearch/>
-            <SectionServicios/>
-            <SectionPuntoGob/>
-            <SectionTramites/>
-            <SectionPreguntas/>
-            <SectionComentarios/>
-        </main>
-        <Footer/>
-        <ChatBotContainer/>
+    <body
+      onTouchMove={() => {
+        SetMenu();
+      }}
+    >
+      <Header />
+      <main>
+        <SectionSearch />
+        <SectionInputSearch />
+        <Section_MapGoogle />
+        <SectionServicios />
+        <SectionPuntoGob />
+        <SectionTramites />
+        <SectionPreguntas />
+        <SectionComentarios />
+      </main>
+      <Footer />
+      <ChatBotContainer />
     </body>
-  )
+  );
 }
