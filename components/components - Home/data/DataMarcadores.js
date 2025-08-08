61025d1446750ff644da5d38ea9275fd9a1e31b2
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-08 11:57:32 -0400

    Se agrego un array de datos con los diversos punto GOB

---
+++ components/components - Home/data/DataMarcadores.js
@@ -0,0 +1,45 @@
+
+export const DataMarcadores = [
+    {
+      NombrePg: "Punto GOB Megacentro",
+      localizacion: {
+        lat: 18.506456694907435,
+        lng: -69.85606654403979,
+      },
+    },
+    {
+      NombrePg: "Punto GOB San Cristóbal",
+      localizacion: {
+        lat: 18.42327811559933,
+        lng: -70.0991001512923,
+      },
+    },
+    {
+      NombrePg: "Punto GOB Occidental Mall",
+      localizacion: {
+        lat: 18.485351665435633,
+        lng: -70.00003049470853,
+      },
+    },
+    {
+      NombrePg: "Punto GOB Sambil",
+      localizacion: {
+        lat: 18.482522,
+        lng: -69.911391,
+      },
+    },
+    {
+      NombrePg: "Punto GOB Santiago",
+      localizacion: {
+        lat: 19.48447232600464,
+        lng: -70.70892480353132,
+      },
+    },
+    {
+      NombrePg: "Punto GOB Expreso",
+      localizacion: {
+        lat: 18.481897965924357,
+        lng: -69.8457521863564,
+      },
+    },
+  ];
