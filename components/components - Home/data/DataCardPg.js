a87ac27d8bfbb52060b3db35273fa0723bb29297
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-08 11:57:12 -0400

    SSe agrego un array de datos para las cartas de estadisticas

---
+++ components/components - Home/data/DataCardPg.js
@@ -0,0 +1,23 @@
+
+export const DataCardPg = [
+    {
+        Titulo:'Punto GOB - Megacentro',
+        Porcentaje: 71,
+    },
+    {
+        Titulo:'Punto GOB - Megacentro',
+        Porcentaje: 20,
+    },
+    {
+        Titulo:'Punto GOB - Megacentro',
+        Porcentaje: 90,
+    },
+    {
+        Titulo:'Punto GOB - Megacentro',
+        Porcentaje: 51,
+    },
+    {
+        Titulo:'Punto GOB - Megacentro',
+        Porcentaje: 28,
+    }
+]
