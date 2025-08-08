97d85e5b15d88d9d9d3ff9b07c21b035f933ae5d
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-08 11:56:23 -0400

    Se hicieron mejoras en el estilo y se organizo e codigo

--- components/components - Home/components/Section-comentarios.jsx
+++ components/components - Home/components/Section-comentarios.jsx
@@ -1,35 +1,40 @@
-'use client'
+"use client";
 import { IoIosArrowForward } from "react-icons/io";
 import { IoIosArrowBack } from "react-icons/io";
 import CardComentarios from "../SubComponents/Card-comentarios";
-import { Carousel } from 'primereact/carousel';
+import { Carousel } from "primereact/carousel";
 import { DataCardComentarios } from "../data/DataCardComentarios";
 
 export default function SectionComentarios() {
+  const responsiveOptions = [
+    {
+      breakpoint: "667px",
+      numVisible: 1,
+    },
+  ];
 
-     const responsiveOptions = [
-        {
-            breakpoint: '667px',
-            numVisible: 1,
-        },
-     ];
-
-    return(
-        <section className="py-10 flex flex-col gap-10">
-            <div className="text-center">
-                <h1 className="text-4xl">“Transformando el servicio contigo”</h1>
-            </div>
-             <div  id="CardContainer" className="flex  w-full md:px-10 justify-center" style={{scrollbarWidth:'none'}}>
-                <Carousel 
-                value={DataCardComentarios} 
-                numVisible={3} className=" w-[100%]   " 
-                autoplayInterval={3000}
-                responsiveOptions={responsiveOptions}
-                containerClassName="flex gap-10"
-                nextIcon={<IoIosArrowForward  className="text-4xl text-primary"/>}
-                prevIcon={<IoIosArrowBack className="text-4xl text-primary"/>}
-                itemTemplate={(D)=><CardComentarios Data={D}/>}/>
-            </div>
-        </section>
-    )
-};
+  return (
+    <section className="py-10 flex flex-col gap-10">
+      <div className="text-center">
+        <h1 className="text-4xl">“Transformando el servicio contigo”</h1>
+      </div>
+      <div
+        id="CardContainer"
+        className="flex  w-full md:px-10 justify-center"
+        style={{ scrollbarWidth: "none" }}
+      >
+        <Carousel
+          value={DataCardComentarios}
+          numVisible={3}
+          className=" w-[100%]   "
+          autoplayInterval={3000}
+          responsiveOptions={responsiveOptions}
+          containerClassName="flex gap-10"
+          nextIcon={<IoIosArrowForward className="text-4xl hidden sm:flex text-primary" />}
+          prevIcon={<IoIosArrowBack className="text-4xl hidden sm:flex text-primary" />}
+          itemTemplate={(D) => <CardComentarios Data={D} />}
+        />
+      </div>
+    </section>
+  );
+}
