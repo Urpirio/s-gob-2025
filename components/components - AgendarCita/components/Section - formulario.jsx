8350ba3f9c5c31cd6949d9b93a1e735036b65c6b
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-11 09:19:17 -0400

    Se hicieron mejoras al formulario

--- components/components - AgendarCita/components/Section - formulario.jsx
+++ components/components - AgendarCita/components/Section - formulario.jsx
@@ -1,36 +1,35 @@
-'use client';
-import FormPart1 from "../subcomponents/FormPart1";
-import FormPart2 from "../subcomponents/FormPart2";
-import FormPart3 from "../subcomponents/FormPart3";
+"use client";
+import { useFormulario } from "../Hooks/useFormulario";
+import Dialog_ParaQuien from "../subcomponents/Dialog_ParaQuien";
+import FormPart1 from "../subcomponents/C-Formulario/FormPart1";
+import FormPart2 from "../subcomponents/C-Formulario/FormPart2";
+import FormPart3 from "../subcomponents/C-Formulario/FormPart3";
 import { Albert_Sans } from "next/font/google";
-import { useState } from "react";
 
 const albert_Sans = Albert_Sans({
-  subsets: ['latin-ext']
+  subsets: ["latin-ext"],
 });
 
 export default function SectionFormulario() {
+  const {
+    InputValues,
+    setInputValues,
+    setStatusDialogPQuien,
+    StatusDialogPQuien,
+  } = useFormulario();
 
-
-  const [InputValues,setInputValues] = useState({
-    Apellidos: String(),
-    Telefono: String(),
-    Nombres: String(),
-    Correo:String(),
-    Cedula:Number(),
-    Direccion:String(),
-  });
 
   return (
-    <section className={`flex justify-around md:p-10 py-10 md:py-10 flex-wrap ${albert_Sans.className}`}>
-            <FormPart2
-            InputValues={InputValues} 
-            setInputValues={setInputValues}/>
-            <FormPart1 
-            InputValues={InputValues} 
-            setInputValues={setInputValues}/>
-            <FormPart3 
-            InputsForm={InputValues}/>
+    <section
+      className={`flex justify-around md:p-10 py-10 md:py-10 flex-wrap ${albert_Sans.className}`}
+    >
+      <FormPart2 InputValues={InputValues} setInputValues={setInputValues} />
+      <FormPart1 InputValues={InputValues} setInputValues={setInputValues} />
+      <FormPart3 InputsForm={InputValues} />
+      <Dialog_ParaQuien
+        StatusDialog={StatusDialogPQuien}
+        setStatusDialog={setStatusDialogPQuien}
+      />
     </section>
-  )
-};
+  );
+}
